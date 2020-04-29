7bd897e5134936d65e1ce2612d5a18816c2d1a89
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2020-04-29 12:54:19 +0200

    added AccessPoint feature to nodeA

--- LoRaMessengerNodeA/LoRaMessengerNodeA.ino
+++ LoRaMessengerNodeA/LoRaMessengerNodeA.ino
@@ -1,6 +1,7 @@
 #include <SPI.h>
 #include <LoRa.h>
 #include <Wire.h>  
+#include <WiFi.h>
 #include "SSD1306.h" 
 
 
@@ -19,6 +20,12 @@ long lastSendTime = 0;
 int interval = 2000;
 int count = 0;
 
+// Replace with your network credentials
+const char* ssid     = "ESP32AccessPoint";
+const char* password = "12345678";
+
+WiFiServer server(80);
+String header;
 
 void setup() {
   Serial.begin(9600);
@@ -35,7 +42,17 @@ void setup() {
    //LoRa.onReceive(cbk);
   LoRa.receive();
   Serial.println("Initialized");
+
+   // Connect to Wi-Fi network with SSID and password
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
   
+  server.begin();
 
 }
 
@@ -53,6 +70,66 @@ void loop() {
     }
   
     receiveMessage(LoRa.parsePacket());
+
+    WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+ 
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP32 Web Server</h1>");
+            
+  
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
 
 
